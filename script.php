e217ab9b05e06f03c56f6d3061081212376f925c
Muhammad Farooq Afzal <mafzal.bese21seecs@seecs.edu.pk>
2023-12-04 20:11:47 +0500

    add functions to extract links and manipulate urls

--- script.php
+++ script.php
@@ -9,7 +9,7 @@ class WebCrawler {
     public function __construct($seedUrl) {
         $this->urlQueue[] = $seedUrl;
     }
- // Starting point for crawla
+ // Starting point for crawler
     public function crawl() {
         while (!empty($this->urlQueue)) {
             $currentUrl = array_shift($this->urlQueue);
@@ -18,7 +18,7 @@ class WebCrawler {
             if (!in_array($currentUrl, $this->visitedUrls)) {
                 $htmlContent = $this->fetchContent($currentUrl);
 
-                // Branch executed in case of successful html retireval 
+                // Branch executed in case of successful html retrieval  
                 if ($htmlContent !== false) {
                     $this->parseAndExtractData($htmlContent);
                     $this->visitedUrls[] = $currentUrl;
@@ -27,3 +27,75 @@ class WebCrawler {
             }
         }
     }
+}
+
+    // Fetch Content Method sends an HTTP request and retrieves HTML content
+    private function fetchContent($url) {
+        $htmlContent = file_get_contents($url);
+
+        // Handle error if content cannot be fetched
+        if ($htmlContent === false) {
+            echo "Error fetching content from: $url\n";
+        }
+
+        return $htmlContent;
+    }
+
+    // Parse and Extract Data Method extracts data from HTML content
+    private function parseAndExtractData($htmlContent) {
+        // Extract title information (demo: <h1> tag)
+        preg_match('/<h1>(.*?)<\/h1>/', $htmlContent, $matches);
+
+        if (!empty($matches[1])) {
+            // Display or log the extracted data
+            echo "Title: " . $matches[1] . "\n";
+        }
+    }
+
+    // Extract Links Method extracts hyperlinks from HTML content
+    private function extractLinks($htmlContent, $currentUrl) {
+        $dom = new DOMDocument;
+        @$dom->loadHTML($htmlContent); // Suppress warnings
+
+        $links = $dom->getElementsByTagName('a');
+        foreach ($links as $link) {
+            $url = $link->getAttribute('href');
+
+            // Convert relative URLs to absolute URLs
+            $url = $this->makeAbsoluteUrl($url, $currentUrl);
+
+            // Ensure the depth limit is not exceeded
+            $depth = $this->calculateDepth($currentUrl);
+            if ($depth <= $this->depthLimit && !in_array($url, $this->urlQueue) && !in_array($url, $this->visitedUrls)) {
+                $this->urlQueue[] = $url;
+            }
+        }
+    }
+
+    // Make Absolute URL Method converts relative URLs to absolute URLs
+    private function makeAbsoluteUrl($url, $baseUrl) {
+        $urlComponents = parse_url($url);
+
+        // If the URL is relative, convert to absolute
+        if (empty($urlComponents['scheme'])) {
+            $baseUrlComponents = parse_url($baseUrl);
+            $url = $baseUrlComponents['scheme'] . '://' . $baseUrlComponents['host'] . $url;
+        }
+
+        return $url;
+    }
+
+    // Calculate Depth Method calculates the depth of a URL in relation to the seed URL
+    private function calculateDepth($url) {
+        $urlParts = parse_url($url);
+        $path = $urlParts['path'] ?? '';
+        $depth = count(array_filter(explode('/', $path)));
+
+        return $depth;
+    }
+}
+
+// Example usage
+$seedUrl = "https://www.webmd.com/eye-health/nearsightedness-myopia";
+$crawler = new WebCrawler($seedUrl);
+$crawler->crawl();
