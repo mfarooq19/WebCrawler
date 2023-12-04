ace1611a25ed040a69e5265c3b28e95044e66c0e
Muhammad Farooq Afzal <mafzal.bese21seecs@seecs.edu.pk>
2023-12-04 19:52:10 +0500

    Develop crawl method

--- script.php
+++ script.php
@@ -9,3 +9,21 @@ class WebCrawler {
     public function __construct($seedUrl) {
         $this->urlQueue[] = $seedUrl;
     }
+ // Starting point for crawla
+    public function crawl() {
+        while (!empty($this->urlQueue)) {
+            $currentUrl = array_shift($this->urlQueue);
+
+            // Check prev visits, if any, to url
+            if (!in_array($currentUrl, $this->visitedUrls)) {
+                $htmlContent = $this->fetchContent($currentUrl);
+
+                // Branch executed in case of successful html retireval 
+                if ($htmlContent !== false) {
+                    $this->parseAndExtractData($htmlContent);
+                    $this->visitedUrls[] = $currentUrl;
+                    $this->extractLinks($htmlContent, $currentUrl);
+                }
+            }
+        }
+    }
